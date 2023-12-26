9d2cb5c4ad3fbf66ecd65c45dff3a4701f07ca87
younghee <able02@naver.com>
2023-12-26 17:46:25 +0900

    커피그루나루 2일차

--- script/myscript.js
+++ script/myscript.js
@@ -28,6 +28,24 @@ $(function(){
         arrows:true
     })//
 
+//  section4 슬릭
+
+$(".s4_left_move").slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true,
+    arrows:false,
+    slidesToScroll:2
+})
+
+// sns 탭 메뉴
+
+$(".section5_inner ul li").click(function(e){
+    e.preventDefault();
+    $(".section5_inner ul li").toggleClass("on")
+
+})//click
+
 
 
 
